e7a21719d91018f415649e13049fadb78b706d8b
sam bacha <sam@manifoldfinance.com>
2021-11-15 02:49:16 -0800

    v0.0.0

---
+++ _pages/about/zh.md
@@ -0,0 +1,1 @@
+Copyright 2021 - All Rights Reserved
---
+++ prebuild.sh
@@ -0,0 +1,20 @@
+!/bin/bash
+
+bundle update
+
+rm -rf _sass/lib && mkdir -p _sass/lib
+
+mkdir -p _sass/lib/@primer/css
+mkdir -p _sass/lib/font-awesome
+mkdir -p _sass/lib/rouge
+mkdir -p _sass/lib/material-design-lite
+
+# @primer/css
+cp -r node_modules/@primer/css/support    _sass/lib/@primer/css
+cp -r node_modules/@primer/css/base       _sass/lib/@primer/css
+cp -r node_modules/@primer/css/breadcrumb _sass/lib/@primer/css
+cp -r node_modules/@primer/css/buttons    _sass/lib/@primer/css
+cp -r node_modules/@primer/css/forms      _sass/lib/@primer/css
+cp -r node_modules/@primer/css/loaders    _sass/lib/@primer/css
+cp -r node_modules/@primer/css/markdown   _sass/lib/@primer/css
+cp -r node_modules/@primer/css/utilities  _sass/lib/@primer/css
